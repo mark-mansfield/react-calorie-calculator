df74ed8f0210856e5e3c3bf7f4f824be3616b710
mark-mansfield <markgmansfield@gmail.com>
2019-10-07 13:40:19 +1100

    Updates

---
+++ precache-manifest.c0d7a60a66e76ccce5e06511bf586aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5606058fdacc3ba86f67ea971dfe4af6",
+    "url": "/react-calorie-calculator/index.html"
+  },
+  {
+    "revision": "df6aa4e55aca91763f14",
+    "url": "/react-calorie-calculator/static/css/main.7132c218.chunk.css"
+  },
+  {
+    "revision": "60a303de6fce173082a9",
+    "url": "/react-calorie-calculator/static/js/2.9be128c9.chunk.js"
+  },
+  {
+    "revision": "df6aa4e55aca91763f14",
+    "url": "/react-calorie-calculator/static/js/main.92604d5c.chunk.js"
+  },
+  {
+    "revision": "1039a02456845020a594",
+    "url": "/react-calorie-calculator/static/js/runtime-main.fcf3b87e.js"
+  },
+  {
+    "revision": "e462397b05f7de95de4215b6e815a860",
+    "url": "/react-calorie-calculator/static/media/avatar.e462397b.png"
+  }
+]);
